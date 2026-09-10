80862aab98115755df6bd6a22d1ee8efcdf49960
Anup Chauhan <84135396+anupchauhanak6@users.noreply.github.com>
2026-09-10 12:18:24 +0530

    Add comments to API route mappings for clarity

--- FirstProject/BasicAPIsPoints/Program.cs
+++ FirstProject/BasicAPIsPoints/Program.cs
@@ -1,12 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// routes
+app.MapGet("/", () => "Hello World!"); // get
 
-app.MapPost("/post", () => "Post");
+app.MapPost("/post", () => "Post"); // post
 
-app.MapPut("/put", () => "Edit the file");
+app.MapPut("/put", () => "Edit the file"); // put (edit)
 
-app.MapDelete("/delete", () => "Deleted");
+app.MapDelete("/delete", () => "Deleted"); // delete
 
 app.Run();

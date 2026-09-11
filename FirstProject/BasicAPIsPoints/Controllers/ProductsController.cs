921cfa32ebe969bb6fa1c7add5480762b084cee0
Anup Chauhan <84135396+anupchauhanak6@users.noreply.github.com>
2026-09-11 09:31:53 +0530

    Implement GET endpoint for individual products and refactor existing product list method

--- FirstProject/BasicAPIsPoints/Controllers/ProductsController.cs
+++ FirstProject/BasicAPIsPoints/Controllers/ProductsController.cs
@@ -8,11 +8,26 @@ namespace BasicAPIsPoints.Controllers
 
     public class ProductsController : ControllerBase
     {
+        public List<string> fruits = new List<string>{ "Apple", "Banana", "Orange" };
+        // 1. old method: for all products (api/products)
         [HttpGet]
-
         public ActionResult<List<string>> Get()
         {
-            return new List<string> { "Apple", "Banana", "Orange" };
+            return Ok(fruits);
+        }
+
+        // new method: for any one product (api/products/1)
+        [HttpGet("{id}")]
+        public ActionResult<string> GetById(int id)
+        {
+
+            int index = id - 1;
+
+            if (index < 0 || index >= fruits.Count)
+            {
+                return NotFound($"Product with ID {id} nahi mila!");
+            }
+            return Ok(fruits[index]);
         }
     }
 }
